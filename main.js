5ab45b7409e263de63bbc4419888b88d83fc72d7
Calico Randall <calicorrandall@gmail.com>
2019-03-06 13:33:47 -0800

    maybe final commit ?

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let circles = [];
-let numberOfCircles = 1000;
+let numberOfCircles = 5000;
 let whichStartingCircle = 0;
 let clicked = false;
 let average;
 let shade;
+let wallColor;
 
 function setup(){
 	createCanvas(windowWidth,windowHeight);
@@ -36,9 +37,10 @@ class Circle {
     this.x=width/2;
     this.y=height/2;
     this.smallness=random(50);
-    this.xSpeed=random(-2,3);
-    this.ySpeed=random(-2,3);
+    this.xSpeed=random(-5,5);
+    this.ySpeed=random(-5,5);
     this.visible=false;
+    this.white=false;
   }
 
   display(){
@@ -46,25 +48,38 @@ class Circle {
     if (shade<100){
       shade=shade+100;
     }
+    wallColor=0;
+    if (this.white===true || this.x<=0 || this.x>=width || this.y<=0 || this.y>=height){
+      wallColor=255;
+    }
+ 
     noStroke();
-    fill(0,0,255,shade);
-    ellipse(this.x-5,this.y-5,this.smallness,this.smallness);
+    fill(wallColor,wallColor,255,shade);
+    ellipse(this.x-4,this.y-4,this.smallness,this.smallness);
     noStroke();
-    fill(255,0,0,shade);
+    fill(255,wallColor,wallColor,shade);
     ellipse(this.x, this.y, this.smallness, this.smallness);
   }
 
   move(){
-    average=random(25);
-    if (average<21){
-      average=21;
-    }
+    // average=random(25);
+    // if (average<21){
+    //   average=21;
+    // }
     if (mouseIsPressed){
-      this.x=(this.x*20+mouseX)/average;
-      this.y=(this.y*20+mouseY)/average;
+      this.x=(this.x*20+mouseX)/21;
+      this.y=(this.y*20+mouseY)/21;
     }else{
       this.x = this.x + this.xSpeed;
       this.y = this.y + this.ySpeed;
+    }
+
+    if (this.x>=width || this.y>=height || this.x<=0 || this.y<=0){
+        this.x=this.x-this.xSpeed;
+        this.y=this.y-this.ySpeed;
+        this.white=true;
+    }else{
+      this.white=false;
     }
   }
 
